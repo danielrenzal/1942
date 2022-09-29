48d9866ed2f741cb93e7264613c3394bcc5b0315
danielrenzal <57828719+danielrenzal@users.noreply.github.com>
2022-09-29 11:56:53 +0800

    added some comments

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     /** image elements */
     const heroIcon = document.getElementById("hero");
+
     const enemy1Icon = document.createElement("div");
     enemy1Icon.classList.add("enemy1");
+
     const enemy2Icon = document.createElement("div");
     enemy2Icon.classList.add("enemy2");
+
     const bulletIcon = document.createElement("div");
     bulletIcon.classList.add("bullet");
 
+    
     /** container element */
     const enemiesContainer = document.querySelector(".enemies");
     const bulletsContainer = document.querySelector(".bullets");
@@ -18,11 +22,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const scoreDisplay = document.querySelector(".score");
     let scoreValue = 0;
 
+    /** sounds */
     const shootSound = document.querySelector(".shoot");
     const explodeSound = document.querySelector(".explode");
     const collisionSound = document.querySelector(".collide");
 
-    /** class to generate the hero, enemies and bullets */
+
+    /** class to generate the hero, enemies and bullets
+     *  x & y: positions
+     *  icon: their html element
+     *  interval: to save their setInterval ID for when it needs to be cleared
+     */
     class Element{
         constructor(x, y, icon, interval = null){
             this.x = x;
@@ -57,42 +67,51 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(hero.x > 10) hero.x -= 10;
                 break;
             case "Space":
+                /** play sound effect */
                 const new_shot = shootSound.cloneNode();
                 new_shot.play();
+
                 /** generate bullet then append to the DOM */
                 const bullet = new Element(hero.x+7, hero.y-7, bulletIcon.cloneNode());
                 bullet.positionElement();
                 bulletsContainer.appendChild(bullet.icon);
 
                 /** move the bullet upwards */
-                const bulletInterval = setInterval(() => {
+                bullet.interval = setInterval(() => {
                     bullet.y -= 3;
                     bullet.positionElement();
 
                     /** remove from the DOM if it reached the top */
                     if(bullet.y <= 0){
-                        clearInterval(bulletInterval);
+                        clearInterval(bullet.interval);
                         bullet.icon.remove();
                     }
 
                     /** if it collides with an enemy... */
                     enemies.forEach((enemy, index) => {
                         if(Math.abs(bullet.x - enemy.x) < 20 && Math.abs(bullet.y - enemy.y) < 10){
+                            /** play sound effect */
                             const new_explode = explodeSound.cloneNode();
-                             new_explode.play();
-                            clearInterval(bulletInterval);
+                            new_explode.play();
+                            /** stop moving the bullet then remove from the DOM */
+                            clearInterval(bullet.interval);
                             bullet.icon.remove();
+                            /** change the image of the enemy from airplane to exploding (using class) */
                             enemy.icon.classList.remove("enemy");
                             enemy.icon.classList.add("explode");
+                            /** push backwards slightly */
                             enemy.y -= 10;
 
+                            /** after awhile, return the enemy to original state to appear like it's
+                             * a new enemy coming
+                             */
                             setTimeout(() => {
                                 enemy.icon.classList.add("enemy");
                                 enemy.icon.classList.remove("explode");
                                 enemy.y = 0;
                             }, 100);
 
-                        
+                            /** increase the score then display */
                             scoreValue += 10;
                             scoreDisplay.textContent = scoreValue;
                         }
@@ -105,6 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
         /** every keydown, position the hero element */
         hero.positionElement();
     });
+
 
     const enemies = [
         new Element(10, 0, enemy1Icon.cloneNode()),
@@ -123,7 +143,10 @@ document.addEventListener("DOMContentLoaded", () => {
             enemy.y += 2;
             enemy.positionElement();
     
-            /** if enemy reached the bottom part of the view/background/canvas */
+            /** if enemy reached the bottom part of the view/background/canvas,
+             * place it again to the top to a different x coordinate to look like
+             * it's a new enemy coming
+             */
             if(enemy.y == 424){
                 enemy.y = 0;
                 enemy.x = Math.random() * 310;
@@ -131,11 +154,16 @@ document.addEventListener("DOMContentLoaded", () => {
     
             /** if the enemy collides with the hero */
             if(Math.abs(enemy.x - hero.x) <= 20 && Math.abs(enemy.y - hero.y) <= 0){
+                /** play sound effect */
                 const new_collision = collisionSound.cloneNode();
                 new_collision.play();
+                /** decrease score then display */
                 scoreValue -= 500;
                 scoreDisplay.textContent = scoreValue;
     
+                /** if the score reached zero, show the game over sign then stop the 
+                 * enemies from moving using the clearInterval method
+                 */
                 if(scoreValue <= 0){
                     scoreDisplay.textContent = 0;
                     gameOverSign.style.display = "block";
